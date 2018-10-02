4800967620b62e595ad655f5e844faf64bf93943
Gabriel Carvalho <gabriel.fonseca@ccc.ufcg.edu.br>
2018-10-02 07:41:01 -0300

    update 1

--- src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -8,19 +8,45 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 	@Override
 	public void insertFirst(T element) {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(isEmpty()) {
+			this.head.setData(element);
+			this.head.setNext(new DoubleLinkedListNode<T>());
+			this.last.setData(head.getData());
+			this.last.setNext(new DoubleLinkedListNode<T>());
+		} else {
+			if(last.getPrevious().isNIL()) {
+				DoubleLinkedListNode<T> aux = new DoubleLinkedListNode<T>(element,   new DoubleLinkedListNode<T>(), new DoubleLinkedListNode<T>());
+				aux.setNext(this.getHead());
+				
+				this.head = aux;
+			} else {
+			DoubleLinkedListNode<T> aux = new DoubleLinkedListNode<T>(element,   new DoubleLinkedListNode<T>(), new DoubleLinkedListNode<T>());
+			aux.setNext(this.getHead());
+			this.head = aux;
+			}
+		}
 	}
 
 	@Override
 	public void removeFirst() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.head.equals(this.last)) {
+			this.head = new SingleLinkedListNode<T>();
+			this.last = new DoubleLinkedListNode<T>();
+		} else {
+		this.head = this.head.getNext();
+		}
 	}
 
 	@Override
 	public void removeLast() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.head.equals(this.last)) {
+			this.head = new SingleLinkedListNode<T>();
+			this.last = new DoubleLinkedListNode<T>();
+		} else {
+			
+		}
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
--- src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -11,37 +11,74 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if(head.isNIL()) {
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		int sum = 0;
+		SingleLinkedListNode<T> aux = head;
+		while(!(aux.isNIL())) {
+			sum++;
+			aux = aux.getNext();
+		}
+		return sum;
+		
 	}
 
 	@Override
 	public T search(T element) {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> aux2 = head;
+		
+		while(!(aux2.isNIL()) && !(aux2.getData().equals(element))) {
+			aux2 = aux2.getNext();
+		}
+		return aux2.getData();
 	}
 
 	@Override
 	public void insert(T element) {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if(isEmpty()) {
+			this.head.setData(element);
+			this.head.setNext(new SingleLinkedListNode<T>());
+		} else {
+			SingleLinkedListNode<T> aux3 = head;
+			
+			while(!(aux3.getNext().isNIL())) {
+				aux3 = aux3.getNext();
+			}
+			aux3.setNext(new SingleLinkedListNode<T>(element,new SingleLinkedListNode<T>()));
+		}
 	}
 
 	@Override
 	public void remove(T element) {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> aux4 = head;
+		
+		while(!(aux4.getNext().getNext().isNIL())) {
+			aux4 = aux4.getNext();
+		}
+		aux4.setNext(new SingleLinkedListNode<T>());
 	}
 
 	@Override
 	public T[] toArray() {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		Object[] array = (T[]) new Object[size()];
+		SingleLinkedListNode<T> aux5 = head;
+		for(int  i = 0; i < array.length; i++) {
+			array[i] = aux5.getData();
+			aux5 = aux5.getNext();
+		}
+		
+		return (T[]) array;
 	}
 
 	public SingleLinkedListNode<T> getHead() {
